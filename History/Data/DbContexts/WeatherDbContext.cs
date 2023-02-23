68a76868d22a7b910e2419e3e49e945f8f01d3b8
Omar <zaurz7591@gmail.com>
2023-02-23 16:18:42 +0400

    EF homework#1

---
+++ History/Data/DbContexts/WeatherDbContext.cs
@@ -0,0 +1,29 @@
+﻿using History.Data.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace History.Data.DbContexts
+{
+    public class WeatherDbContext:DbContext
+    {
+            public DbSet<ForecastHistory> ForecastHistory { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                ConfigurationBuilder builder = new();
+
+                builder.AddJsonFile("appsettings.json");
+
+                IConfigurationRoot config = builder.Build();
+
+                var connectionString = config.GetConnectionString("History");
+
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+    }
+}
---
+++ History/Data/Model/ForecastHistory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace History.Data.Model
+{
+    public class ForecastHistory
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Icon { get; set; }
+        public string CityName { get; set; }
+        public float Temp { get; set; }
+        public float Humidity { get; set; }
+        public float FeelsLike { get; set; }
+        public float WindSpeed { get; set; }
+        public float Pressure { get; set; }
+    }
+}
